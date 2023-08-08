9fe5a61738fadbd99c6d38d264de9ab2dce27446
Alex Kiura <kiuraalex@gmail.com>
2023-08-08 21:20:18 +0300

    Add simple app to test WSGI

---
+++ app.py
@@ -0,0 +1,18 @@
+from wsgiref.simple_server import make_server
+
+
+def application(environ, start_response):
+    response_body = [
+        f"{key}: {value}" for key, value in sorted(environ.items())
+    ]
+    response_body = "\n".join(response_body)
+    status = "200 OK"
+    response_headers = [
+        ("Content-Type", "text/plain"),
+    ]
+    start_response(status, response_headers)
+    return [response_body.encode("utf-8")]
+
+
+server = make_server("localhost", 8000, app=application)
+server.serve_forever()
