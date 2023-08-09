8ed1044494f5a280ebaf0e9c45f73b6eb05d3072
Alex Kiura <kiuraalex@gmail.com>
2023-08-10 02:43:22 +0300

    Use API class

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
-from wsgiref.simple_server import make_server
-from middleware import Reverseware
+from api import API
 
 
-def application(environ, start_response):
-    response_body = [
-        f"{key}: {value}" for key, value in sorted(environ.items())
-    ]
-    response_body = "\n".join(response_body)
-    status = "200 OK"
-    response_headers = [
-        ("Content-Type", "text/plain"),
-    ]
-    start_response(status, response_headers)
-    return [response_body.encode("utf-8")]
-
-
-server = make_server("localhost", 8000, app=Reverseware(application))
-server.serve_forever()
+app = API()
