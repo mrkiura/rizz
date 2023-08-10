647796b8c7c97be9c2951e1babd39432314f28a3
Alex Kiura <kiuraalex@gmail.com>
2023-08-10 03:26:32 +0300

    Add find handler method

--- api.py
+++ api.py
@@ -10,14 +10,18 @@ class API:
         response = self.handle_request(request)
         return response(environ, start_response)
 
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            if path == request_path:
+                return handler
+
     def handle_request(self, request):
         response = Response()
-        for path, handler in self.routes.items():
-            if path == request.path:
-                handler(request, response)
-                return response
-
-        self.default_response(response)
+        handler = self.find_handler(request_path=request.path)
+        if handler:
+            handler(request, response)
+        else:
+            self.default_response(response)
         return response
 
     def route(self, path):
