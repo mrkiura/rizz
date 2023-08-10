14b2d5f9652e81cf84301f4062aa44fcf9b6ddaf
Alex Kiura <kiuraalex@gmail.com>
2023-08-10 03:13:38 +0300

    Add home and about pages

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ from api import API
 
 
 app = API()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
