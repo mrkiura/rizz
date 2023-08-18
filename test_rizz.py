334beae175b55ae0d5da6f5e0cff232a4eb3de00
Alex Kiura <kiuraalex@gmail.com>
2023-08-19 02:06:37 +0300

    Test template rendering

--- test_rizz.py
+++ test_rizz.py
@@ -98,3 +98,16 @@ def test_alternative_route(api, client):
     api.add_route("/alternative", home)
 
     assert client.get("http://testserver/alternative").text == RESPONSE_TEXT
+
+
+def test_template(api, client):
+    @api.route("/html")
+    def html_handler(req, resp):
+        resp.body = api.template(
+            "index.html", context={"title": "Some Title", "name": "Some Name"}
+        ).encode()
+
+    response = client.get("http://testserver/html")
+    assert "text/html" in response.headers["Content-Type"]
+    assert "Some Title" in response.text
+    assert "Some Name" in response.text
