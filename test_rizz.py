bd2e28eae4683a5be81c5a7e3b6147a2d4fa8c40
Alex Kiura <kiuraalex@gmail.com>
2023-08-18 23:04:17 +0300

    Test that routes can be added by calling add_route

--- test_rizz.py
+++ test_rizz.py
@@ -87,3 +87,14 @@ def test_class_based_handler_not_allowed_method(api, client):
 
     with pytest.raises(AttributeError):
         client.get("http://testserver/book")
+
+
+def test_alternative_route(api, client):
+    RESPONSE_TEXT = "Alternative way to add a route"
+
+    def home(req, resp):
+        resp.text = RESPONSE_TEXT
+
+    api.add_route("/alternative", home)
+
+    assert client.get("http://testserver/alternative").text == RESPONSE_TEXT
