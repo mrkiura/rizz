8c8460d466e9db48d980ba8b2aa4107eb2594e9b
Alex Kiura <kiuraalex@gmail.com>
2023-08-19 03:12:25 +0300

    Test assets are served

--- test_rizz.py
+++ test_rizz.py
@@ -3,6 +3,17 @@ import pytest
 from api import API
 
 
+FILE_DIR = "css"
+FILE_NAME = "main.css"
+FILE_CONTENTS = "body {background-color: red}"
+
+
+def _create_static(static_dir):
+    asset = static_dir.mkdir(FILE_DIR).join(FILE_NAME)
+    asset.write(FILE_CONTENTS)
+    return asset
+
+
 @pytest.fixture
 def api():
     return API()
@@ -125,3 +136,19 @@ def test_custom_exception_handler(api, client):
 
     response = client.get("http://testserver/exception")
     assert response.text == "AttributeErrorHappened"
+
+
+def test_404_is_returned_for_non_existent_static_file(client):
+    assert client.get("http://testserver/main.css").status_code == 404
+
+
+def test_assets_are_served(tmpdir_factory):
+    static_dir = tmpdir_factory.mktemp("static")
+    _create_static(static_dir)
+
+    api = API(static_dir=str(static_dir))
+    client = api.test_session()
+
+    response = client.get(f"http://testserver/{FILE_DIR}/{FILE_NAME}")
+    assert response.status_code == 200
+    assert response.text == FILE_CONTENTS
