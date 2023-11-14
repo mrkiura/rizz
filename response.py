67d15638e0af8760b7bbebefd6bcc36bc9c2cf31
Alex Kiura <kiuraalex@gmail.com>
2023-11-14 23:16:13 +0300

    Configure custom request class

--- response.py
+++ response.py
@@ -1,3 +1,7 @@
+from typing import Any
+from webob import Response as WebObResponse
+
+
 class Response:
     def __init__(self) -> None:
         self.json = None
@@ -5,4 +9,12 @@ class Response:
         self.text = None
         self.content_type = None
         self.body = b""
-        self.status_code
+        self.status_code = 200
+
+    def __call__(self, environ, start_response) -> Any:
+        response = WebObResponse(
+            body=self.body,
+            content_type=self.content_type,
+            status=self.status_code
+        )
+        return response(environ, start_response)
