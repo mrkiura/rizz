5bb41899f7063c0c8b26176cf8d430b9ae7f2bdb
Alex Kiura <kiuraalex@gmail.com>
2023-11-14 23:07:56 +0300

    Test manually setting response body in bytes

--- test_rizz.py
+++ test_rizz.py
@@ -231,3 +231,15 @@ def test_text_response_helper(api, client):
 
     assert "text/plain" in response.headers["Content-Type"]
     assert response.text == response.text
+
+
+def test_manually_setting_body(api, client):
+    @api.route("/body")
+    def text_handler(request, response):
+        response.body = b"Byte Body"
+        response.content_type = "text/plain"
+
+    response = client.get("http://testserver/body")
+
+    assert "text/plain" in response.headers["Content-Type"]
+    assert response.text == "Byte Body"
