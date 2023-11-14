238ab411f67fc43cb42fe0d9da367124b9f08291
Alex Kiura <kiuraalex@gmail.com>
2023-11-14 16:20:33 +0300

    Scaffold middleware class

--- middleware.py
+++ middleware.py
@@ -1,3 +1,12 @@
 class Middleware:
     def __init__(self, app) -> None:
         self.app = app
+
+    def add(self, middeware_cls):
+        self.app = middeware_cls(self.app)
+
+    def process_request(self, request):
+        pass
+
+    def process_response(self, request, response):
+        pass
